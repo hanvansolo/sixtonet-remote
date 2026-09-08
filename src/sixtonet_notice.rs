55805646a654e09be2e3853f390ee183be48558b
Ed Deyzel <ed@deyzel.uk>
2026-09-08 02:12:42 +0100

    Collapse support notice on close; keep explicit session stop available

--- src/sixtonet_notice.rs
+++ src/sixtonet_notice.rs
@@ -9,6 +9,15 @@ pub fn start(operator: &str, input: bool) -> hbb_common::ResultType<()> {
     use std::{ptr::null_mut, sync::mpsc, time::Duration};
     use winapi::{shared::windef::HWND, um::{libloaderapi::GetModuleHandleW, winuser::*}};
     fn wide(s: &str) -> Vec<u16> { s.encode_utf16().chain(Some(0)).collect() }
+    unsafe fn compact(hwnd: HWND, small: bool) {
+        let label = GetDlgItem(hwnd, 2);
+        ShowWindow(label, if small { SW_HIDE } else { SW_SHOWNOACTIVATE });
+        SetWindowPos(hwnd, null_mut(), 0, 0, if small { 390 } else { 560 },
+            if small { 90 } else { 145 }, SWP_NOMOVE | SWP_NOZORDER | SWP_NOACTIVATE);
+        MoveWindow(GetDlgItem(hwnd, 1), 12, if small { 8 } else { 60 }, 220, 28, 1);
+        MoveWindow(GetDlgItem(hwnd, 3), 244, if small { 8 } else { 60 }, 120, 28, 1);
+        SetWindowTextW(GetDlgItem(hwnd, 3), wide(if small { "Show details" } else { "Collapse" }).as_ptr());
+    }
     unsafe extern "system" fn proc(hwnd: HWND, msg: u32, w: usize, l: isize) -> isize {
         match msg {
             WM_TIMER => {
@@ -16,7 +25,8 @@ pub fn start(operator: &str, input: bool) -> hbb_common::ResultType<()> {
                 0
             }
             WM_COMMAND if w & 0xffff == 1 => { std::process::exit(74); }
-            WM_CLOSE => { std::process::exit(74); }
+            WM_COMMAND if w & 0xffff == 3 => { compact(hwnd, IsWindowVisible(GetDlgItem(hwnd, 2)) != 0); 0 }
+            WM_CLOSE => { compact(hwnd, true); 0 }
             _ => DefWindowProcW(hwnd, msg, w, l),
         }
     }
@@ -35,10 +45,12 @@ pub fn start(operator: &str, input: bool) -> hbb_common::ResultType<()> {
             20, 20, 560, 145, null_mut(), null_mut(), instance, null_mut());
         if hwnd.is_null() { let _ = tx.send(false); return; }
         let label = CreateWindowExW(0, wide("STATIC").as_ptr(), wide(&text).as_ptr(), WS_CHILD | WS_VISIBLE,
-            12, 8, 530, 45, hwnd, null_mut(), instance, null_mut());
+            12, 8, 530, 45, hwnd, 2 as _, instance, null_mut());
         let button = CreateWindowExW(0, wide("BUTTON").as_ptr(), wide("End support session").as_ptr(), WS_CHILD | WS_VISIBLE | WS_TABSTOP,
             12, 60, 220, 28, hwnd, 1 as _, instance, null_mut());
-        if label.is_null() || button.is_null() || SetTimer(hwnd, 1, 250, None) == 0 {
+        let collapse = CreateWindowExW(0, wide("BUTTON").as_ptr(), wide("Collapse").as_ptr(), WS_CHILD | WS_VISIBLE | WS_TABSTOP,
+            244, 60, 120, 28, hwnd, 3 as _, instance, null_mut());
+        if collapse.is_null() || label.is_null() || button.is_null() || SetTimer(hwnd, 1, 250, None) == 0 {
             DestroyWindow(hwnd); let _ = tx.send(false); return;
         }
         if tx.send(true).is_err() { DestroyWindow(hwnd); return; }
